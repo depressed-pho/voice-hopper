89417603a49c892d9cecf6f2af7257f5c29e896f
PHO <pho@cielonegro.org>
2025-12-24 14:30:54 +0900

    Split src/main.lua

--- gulpfile.ts
+++ gulpfile.ts
@@ -67,6 +67,7 @@ export async function build(): Promise<void> {
                 luaVersion: "LuaJIT",
                 paths: [
                     "lib/?.lua",
+                    "src/?.lua",
                 ],
             });
 
