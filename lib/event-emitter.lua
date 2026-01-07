e7667b1ef2b2f81041c127b9a48f2078eb095ed6
PHO <pho@cielonegro.org>
2026-01-07 17:12:16 +0900

    lib/event-emitter.lua: Implement async subscriptions

--- lib/event-emitter.lua
+++ lib/event-emitter.lua
@@ -37,9 +37,12 @@ local function EventEmitter(base)
         end
     end
 
+    --
+    -- Subscribe to an event with a given function.
+    --
     function klass:on(name, func)
-        assert(isName(name), "EventEmitter:on() expects an event name as its 1st argument")
-        assert(type(func) == "function", "EventEmitter:on() expects a listener function as its 2nd argument")
+        assert(isName(name), "EventEmitter#on() expects an event name as its 1st argument")
+        assert(type(func) == "function", "EventEmitter#on() expects a listener function as its 2nd argument")
 
         if allowedEvents and not allowedEvents[name] then
             error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
@@ -55,9 +58,13 @@ local function EventEmitter(base)
         return self
     end
 
-    function klass:once(name, func)
-        assert(isName(name), "EventEmitter:once() expects an event name as its 1st argument")
-        assert(type(func) == "function", "EventEmitter:once() expects a listener function as its 2nd argument")
+    --
+    -- Asynchronous variant of EventEmitter#on(). The function will be
+    -- evaluated in an asynchronous context.
+    --
+    function klass:onAsync(name, func)
+        assert(isName(name), "EventEmitter#onAsync() expects an event name as its 1st argument")
+        assert(type(func) == "function", "EventEmitter#onAsync() expects a listener function as its 2nd argument")
 
         if allowedEvents and not allowedEvents[name] then
             error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
@@ -69,21 +76,71 @@ local function EventEmitter(base)
             self._listeners[name] = listenersOf
         end
         listenersOf[func] = function(...)
-            local ok, err = pcall(func, ...)
-
-            listenersOf[func] = nil
-
-            if not ok then
-                error(err, 0) -- Don't rewrite the error.
-            end
+            -- Start the coroutine right now. We don't care if it runs till
+            -- the termination or not. If it yields it's expected to be
+            -- resumed by someone else, most likely a promise.
+            coroutine.wrap(func)(...)
         end
 
         return self
     end
 
+    --
+    -- Subscribe to an event with a given function. The function will be
+    -- automatically unsubscribed on the first event it receives.
+    --
+    function klass:once(name, func)
+        assert(isName(name), "EventEmitter#once() expects an event name as its 1st argument")
+        assert(type(func) == "function", "EventEmitter#once() expects a listener function as its 2nd argument")
+
+        if allowedEvents and not allowedEvents[name] then
+            error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
+        end
+
+        local listenersOf = self._listeners[name]
+        if listenersOf == nil then
+            listenersOf = {}
+            self._listeners[name] = listenersOf
+        end
+        listenersOf[func] = function(...)
+            listenersOf[func] = nil
+            func(...)
+        end
+
+        return self
+    end
+
+    --
+    -- Asynchronous variant of EventEmitter#once(). The function will be
+    -- evaluated in an asynchronous context.
+    --
+    function klass:onceAsync(name, func)
+        assert(isName(name), "EventEmitter#once() expects an event name as its 1st argument")
+        assert(type(func) == "function", "EventEmitter#once() expects a listener function as its 2nd argument")
+
+        if allowedEvents and not allowedEvents[name] then
+            error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
+        end
+
+        local listenersOf = self._listeners[name]
+        if listenersOf == nil then
+            listenersOf = {}
+            self._listeners[name] = listenersOf
+        end
+        listenersOf[func] = function(...)
+            listenersOf[func] = nil
+            coroutine.wrap(func)(...)
+        end
+
+        return self
+    end
+
+    --
+    -- Unsubscribe a listener from an event.
+    --
     function klass:off(name, func)
-        assert(isName(name), "EventEmitter:on() expects an event name as its 1st argument")
-        assert(type(func) == "function", "EventEmitter:on() expects a listener function as its 2nd argument")
+        assert(isName(name), "EventEmitter#off() expects an event name as its 1st argument")
+        assert(type(func) == "function", "EventEmitter#off() expects a listener function as its 2nd argument")
 
         if allowedEvents and not allowedEvents[name] then
             error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
@@ -97,8 +154,11 @@ local function EventEmitter(base)
         return self
     end
 
+    --
+    -- Emit an event. Extra arguments are redirected to listener functions.
+    --
     function klass:emit(name, ...)
-        assert(isName(name), "EventEmitter:emit() expects an event name as its 1st argument")
+        assert(isName(name), "EventEmitter#emit() expects an event name as its 1st argument")
 
         if allowedEvents and not allowedEvents[name] then
             error("Event " .. tostring(name) .. " is not available on this EventEmitter", 2)
